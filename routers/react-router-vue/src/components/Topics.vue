81597920f0b06dd7bc77aedee3a140ac0b306a95
syumai <syumai@gmail.com>
2017-10-06 09:07:01 +0900

    Added react-route-vue

---
+++ routers/react-router-vue/src/components/Topics.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <router-link :to="`${match.url}/rendering`">
+          Rendering with Vue
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="`${match.url}/components`">
+          Components
+        </router-link>
+      </li>
+      <li>
+        <router-link :to="`${match.url}/props-v-state`">
+          Props v. State
+        </router-link>
+      </li>
+    </ul>
+
+    <route :path="`${match.url}/:topicId`" :component="Topic" />
+    <route exact :path="match.url" :render="renderElement" />
+  </div>
+</template>
+
+<script>
+import React from 'react';
+
+import {
+  Route,
+  Link as RouterLink // Not working with name 'Link'
+} from 'react-router-dom';
+
+import Topic from './Topic';
+
+export default {
+  props: ['match'],
+
+  data: {
+    Topic
+  },
+
+  components: {
+    Route,
+    RouterLink
+  },
+
+  methods: {
+    renderElement() {
+      return <h3>Please select a topic.</h3>;
+    }
+  }
+}
+
+</script>
---
+++ routers/react-router-vue/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx', '.vue']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.vue$/,
+        loader: 'react-vue-loader'
+      }
+    ]
+  }
+};
+
